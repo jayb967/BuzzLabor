a8a57c7b6988160e59c2c6eeeac822dd1c946755
Rio <jayb967@gmail.com>
2017-06-03 17:05:23 -0700

    added styling, model files and sign-in controller

---
+++ BuzzLabor/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  BuzzLabor
+//
+//  Created by Jay Balderas on 6/3/17.
+//  Copyright © 2017 Jay Balderas. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    // DBProvider
+    static let RIDERS = "riders"
+    static let EMAIL = "email"
+    static let PASSWORD = "password"
+    static let DATA = "data"
+    static let isRider = "isRider"
+    static let UBER_REQUEST = "Uber_Request"
+    static let UBER_ACCEPTED = "Uber_Accepted"
+    
+    // Uber Handler
+    static let NAME = "name"
+    static let LATITUDE = "latitude"
+    static let LONGITUDE = "longitude"
+    
+    
+}
